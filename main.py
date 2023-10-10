2854a50ce09eeb4dc275cef320f4cdd0e3129783
MichealC03 <micheal.callahan@icloud.com>
2023-10-10 11:33:51 -0700

    added notebook

--- main.py
+++ main.py
@@ -1,45 +1,22 @@
-import requests
-import matplotlib.pyplot as plt
+from pyopensky.impala import Impala
+import pandas as pd
 
-# Replace 'YOUR_API_KEY' with your AviationStack API key
-API_KEY = '042753dc1ba431e8e1d24b8a2cb1d7e5'
+# Create an instance of the Impala class with authentication credentials
+impala_instance = Impala(
+    username="a",
+    password="Soccer21!",
+    host="data.opensky-network.org",
+    port=2230  # The port for the Impala shell
+)
 
-# Flight ICAO24 identifier (replace with the desired flight's ICAO24)
-ICAO24 = 'ABFE45'
+hist = impala_instance.history(
+    "2017-02-05 15:45",
+    stop="2017-02-05 16:45",
+    callsign="EZY158T",
+    # returns a Flight instead of a Traffic
+    return_flight=True
+)
 
-# AviationStack API endpoint for flight tracking
-API_ENDPOINT = f'http://api.aviationstack.com/v1/flights?access_key={API_KEY}&icao24={ICAO24}'
+df = pd.DataFrame(hist)
 
-def get_flight_data():
-    try:
-        response = requests.get(API_ENDPOINT)
-        data = response.json()
-        return data['data'][0]  # Assuming we are interested in the first result
-    except Exception as e:
-        print(f"Error fetching flight data: {str(e)}")
-        return None
-
-def plot_flight_position(flight_data):
-    if flight_data:
-        latitude = flight_data['latitude']
-        longitude = flight_data['longitude']
-
-        plt.figure(figsize=(10, 6))
-        plt.scatter(longitude, latitude, c='red', label='Flight Position')
-        plt.xlabel('Longitude')
-        plt.ylabel('Latitude')
-        plt.title('Flight Tracking')
-        plt.legend()
-        plt.grid(True)
-        plt.show()
-
-if __name__ == "__main__":
-    flight_data = get_flight_data()
-    if flight_data:
-        print("Flight Data:")
-        #print(f"Flight ICAO24: {flight_data['icao24']}")
-        #print(f"Flight Callsign: {flight_data['callsign']}")
-        #print(f"Latitude: {flight_data['latitude']}")
-        #print(f"Longitude: {flight_data['longitude']}")
-        print(flight_data)
-        plot_flight_position(flight_data)
+print(df.lat)
